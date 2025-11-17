a5c6388b991ae99c8afa27a482084757370d85a1
idubnori <i.dub.nori@gmail.com>
2025-11-17 09:31:30 +0900

    feat: support to configure AddActionButton

--- mobile/lib/utils/action_button.utils.dart
+++ mobile/lib/utils/action_button.utils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:immich_mobile/constants/enums.dart';
 import 'package:immich_mobile/domain/models/album/album.model.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
+import 'package:immich_mobile/presentation/widgets/action_buttons/add_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/advanced_info_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/archive_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/delete_action_button.widget.dart';
@@ -49,6 +50,7 @@ enum ActionButtonType {
   advancedInfo,
   share,
   edit,
+  add,
   shareLink,
   similarPhotos,
   archive,
@@ -69,6 +71,7 @@ enum ActionButtonType {
     return switch (this) {
       ActionButtonType.advancedInfo => context.advancedTroubleshooting,
       ActionButtonType.share => true,
+      ActionButtonType.add => context.asset.hasRemote,
       ActionButtonType.edit =>
         !context.isInLockedView && //
             context.asset.isImage,
@@ -141,6 +144,7 @@ enum ActionButtonType {
       ActionButtonType.advancedInfo => AdvancedInfoActionButton(source: context.source),
       ActionButtonType.share => ShareActionButton(source: context.source),
       ActionButtonType.edit => const EditImageActionButton(),
+      ActionButtonType.add => const AddActionButton(),
       ActionButtonType.shareLink => ShareLinkActionButton(source: context.source),
       ActionButtonType.archive => ArchiveActionButton(source: context.source),
       ActionButtonType.unarchive => UnArchiveActionButton(source: context.source),
@@ -173,6 +177,7 @@ class ActionButtonBuilder {
     ActionButtonType.share,
     ActionButtonType.upload,
     ActionButtonType.edit,
+    ActionButtonType.add,
     ActionButtonType.archive,
     ActionButtonType.delete,
     ActionButtonType.removeFromAlbum,
